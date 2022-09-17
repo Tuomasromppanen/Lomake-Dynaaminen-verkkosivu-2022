58a1decddd516abf0ea907ed712ebead9e353f80
Tuomasromppanen <tuomas.romppanen@outlook.com>
2022-09-17 09:59:28 +0300

    Alcometer

--- src/App.js
+++ src/App.js
@@ -44,6 +44,8 @@ function App() {
       <option value = "2">2</option>
       <option value = "3">3</option>
       <option value = "4">4</option>
+      <option value = "5">5</option>
+      <option value = "6">6</option>
       </select>
     </div>
     <div>
@@ -53,6 +55,8 @@ function App() {
       <option value = "2">2</option>
       <option value = "3">3</option>
       <option value = "4">4</option>
+      <option value = "5">5</option>
+      <option value = "6">6</option>
       </select>
     </div>
     <div>
